2119b45293aec00ea08829b7a39052c4c001ba98
vcrobotics <aloha1epp@yahoo.com>
2017-01-30 18:25:03 -0800

    Initial manipulator skeleton code

--- 2017Robot/src/org/usfirst/frc/team3256/robot/RobotMap.java
+++ 2017Robot/src/org/usfirst/frc/team3256/robot/RobotMap.java
@@ -7,14 +7,26 @@ package org.usfirst.frc.team3256.robot;
  * floating around.
  */
 public class RobotMap {
+	
+	//PWM Motor
 	public static final int LEFT_FRONT_DRIVE = 9;
 	public static final int LEFT_BACK_DRIVE = 8;
 	public static final int RIGHT_FRONT_DRIVE = 7;
 	public static final int RIGHT_BACK_DRIVE = 6;
+	public static final int HANGER_1 = 4;
+	public static final int HANGER_2 = 5;
+	public static final int INNER_MOTOR_ROLLER = 3;
+	public static final int OUTER_MOTOR_ROLLER = 2;
+	
+	//PCM
+	public static final int BALL_PIVOT_A = 4;
+	public static final int BALL_PIVOT_B = 5;
+	public static final int GEAR_PIVOT_A = 6;
+	public static final int GEAR_PIVOT_B = 7;
+	
+	//Encoder 
 	public static final int ENCODER_LEFT_A = 3;
 	public static final int ENCODER_LEFT_B = 2;
 	public static final int ENCODER_RIGHT_A = 1;
 	public static final int ENCODER_RIGHT_B = 0;
-	public static final int HANGER_1 = 4;
-	public static final int HANGER_2 = 5;
 }
