fb6c2b905924d6d3836da25f7ffa2fbe560c28da
vcrobotics <aloha1epp@yahoo.com>
2017-02-20 18:16:22 -0800

    update constants for competition bot and clean up constants

--- 2017Robot/src/org/usfirst/frc/team3256/robot/Constants.java
+++ 2017Robot/src/org/usfirst/frc/team3256/robot/Constants.java
@@ -2,49 +2,47 @@ package org.usfirst.frc.team3256.robot;
 
 public class Constants {
 
+//------------------------------ELECTRICAL PORTS------------------------------
+	
 	//PWM Motor Controller ports
-	public static final int LEFT_DRIVE = 0;
-	public static final int RIGHT_DRIVE = 1;
-	public static final int HANGER_1 = 6;
-	public static final int HANGER_2 = 7;
-	public static final int INNER_MOTOR_ROLLER = 4;
-	public static final int OUTER_MOTOR_ROLLER = 5;
+	public static final int LEFT_DRIVE = 9; //0
+	public static final int RIGHT_DRIVE = 8; //1
+	public static final int HANGER_1 = 6; //6
+	public static final int HANGER_2 = 7; //7
+	public static final int INNER_MOTOR_ROLLER = 4; //4
+	public static final int OUTER_MOTOR_ROLLER = 5; //5
 	
 	//PDP Motor ports
-	public static final int PDP_LEFT_FRONT = 0;
-	public static final int PDP_RIGHT_FRONT = 1;
-	public static final int PDP_LEFT_BACK = 2;
-	public static final int PDP_RIGHT_BACK = 3;
-	public static final int PDP_HANGER_1 = 4;
-	public static final int PDP_HANGER_2 = 5;
-	public static final int PDP_INNER_MOTOR_ROLLER = 6;
-	public static final int PDP_OUTER_MOTOR_ROLLER = 7;
+	public static final int PDP_LEFT_FRONT = 1; //0
+	public static final int PDP_RIGHT_FRONT = 2; //1
+	public static final int PDP_LEFT_BACK = 0; //2 
+	public static final int PDP_RIGHT_BACK = 3; //3
+	public static final int PDP_HANGER_1 = 4; //4 
+	public static final int PDP_HANGER_2 = 5; //5
+	public static final int PDP_INNER_MOTOR_ROLLER = 6; //6 
+	public static final int PDP_OUTER_MOTOR_ROLLER = 7; //7
 
 	//PCM Solenoid ports
-	public static final int DRIVE_SHIFTER_A = 4;
-	public static final int DRIVE_SHIFTER_B = 3;
-	public static final int BALL_PIVOT_A = 2;
-	public static final int BALL_PIVOT_B = 0;
-	public static final int GEAR_PIVOT_A = 1;
-	public static final int GEAR_PIVOT_B = 5;
+	public static final int DRIVE_SHIFTER_A = 0; //4
+	public static final int DRIVE_SHIFTER_B = 6; //3
+	public static final int BALL_PIVOT_A = 2; //2
+	public static final int BALL_PIVOT_B = 3; //0
+	public static final int GEAR_PIVOT_A = 1; //1
+	public static final int GEAR_PIVOT_B = 5; //5
 
 	//Encoder ports on the Spartan Board 
-	public static final int ENCODER_LEFT_A = 13;
-	public static final int ENCODER_LEFT_B = 12;
-	public static final int ENCODER_RIGHT_A = 11;
-	public static final int ENCODER_RIGHT_B = 10;
-
+	public static final int ENCODER_LEFT_A = 11; //13
+	public static final int ENCODER_LEFT_B = 10; //12
+	public static final int ENCODER_RIGHT_A = 13; //11
+	public static final int ENCODER_RIGHT_B = 12; //10
+	
 	// Joystick ports and deadband
 	public static final int DRIVER_CONTROLLER = 0;
 	public static final int MANIPULATOR_CONTROLLER = 1;
 	public static final double XBOX_DEADBAND_VALUE = 0.25;
+
+//------------------------------Physical Robot Constants------------------------------
 	
-	//Preset Values
-	public static final double GROUND_INTAKE_POWER = 0.5;
-	public static final double SHOOT_BALLS_POWER = -0.5;
-	public static final double WINCH_HANGER_POWER = 0.5;
-	
-	//Physical Robot Constants 
 	public static final double MAX_VEL_HIGH_GEAR_IN_SEC = 15.0 * 12.0; // 18.94 theoretical ft/s
 	public static final double MAX_ACCEL_HIGH_GEAR_IN_SEC2 = 20; 
 	public static final double MAX_VEL_TURN_LOW_GEAR_DEG_SEC = 75.0;
@@ -53,29 +51,32 @@ public class Constants {
 	public static final double WHEEL_DIAMETER = 4.09; //inches -- theoretical 4", includes tread
 	public static final double INCHES_PER_TICK = WHEEL_DIAMETER*Math.PI/GRAYHILL_TICKS_PER_ROT;
 	
-	//Software Constants
+//------------------------------SOFTWARE CONSTANTS------------------------------
+
+	//Preset Values
+	public static final double GROUND_INTAKE_POWER = 0.5;
+	public static final double SHOOT_BALLS_POWER = -0.5;
+	public static final double WINCH_HANGER_POWER = 0.5;
+	
+	//Period for all control loops
 	public static final double CONTROL_LOOP_DT = 0.02;
 	
 	//Turn PID Gains - LOW GEAR
-	
 	public static final double KP_PID_TURN = 0.0178;
 	public static final double KI_PID_TURN = 0.001;
 	public static final double KD_PID_TURN = 0.01;
 	
-	//-------------------DISTANCE MOTION PROFILING-------------------
-	//Distance FeedForward Gains 
+	//Drive Motion Profile Gains - HIGH GEAR
 	public static final double KV_DISTANCE = 1.0/MAX_VEL_HIGH_GEAR_IN_SEC;
 	public static final double KA_DISTANCE = 0.004;
-	//Distance PID Gains
 	public static final double KP_DISTANCE = 0.17;
 	public static final double KI_DISTANCE = 0.0;
 	public static final double KD_DISTANCE = 0.0;
-	//Distance Straight Gains
 	public static final double KP_STRAIGHT = 0.02;
 	public static final double KI_STRAIGHT = 0.0;
 	public static final double KD_STRAIGHT = 0.0;
 	
-	//-------------------TURN MOTION PROFILING-------------------
+	//Turn Motion Profile Gains - LOW GEAR
 	public static final double KV_TURN = 1.0/MAX_VEL_TURN_LOW_GEAR_DEG_SEC;
 	public static final double KA_TURN = 0.002;
 	public static final double KP_TURN = 0.02;
