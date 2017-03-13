ca2548d536591c01018bd5401af3fa44dcf36d4e
Team 3256 <warriorborgs@gmail.com>
2017-03-13 16:07:32 -0700

    add left and right drive towards gear station in auto

--- 2017Robot/src/org/usfirst/frc/team3256/robot/automodes/GearCenterLeftAuto.java
+++ 2017Robot/src/org/usfirst/frc/team3256/robot/automodes/GearCenterLeftAuto.java
@@ -4,16 +4,17 @@ import org.usfirst.frc.team3256.lib.DelayedCommand;
 import org.usfirst.frc.team3256.robot.commands.DeployGear;
 import org.usfirst.frc.team3256.robot.commands.DriveToDistance;
 import org.usfirst.frc.team3256.robot.commands.MotionProfiledTurn;
+import org.usfirst.frc.team3256.robot.commands.PIDTurn;
 import org.usfirst.frc.team3256.robot.commands.ShootBalls;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
- * simply drives to gear and back
+ * drives to gear and back with a left turn towards gear station
  */
-public class GearCenterAuto extends CommandGroup {
+public class GearCenterLeftAuto extends CommandGroup {
 
-	public GearCenterAuto() {
+	public GearCenterLeftAuto() {
 		// drive backwards to center gear peg
 		addSequential(new DriveToDistance(77, false));
 
@@ -21,7 +22,18 @@ public class GearCenterAuto extends CommandGroup {
 		addParallel(new DeployGear());
 		
 		//drive forward
-		addSequential(new DelayedCommand(1,new DriveToDistance(60,true)));
+		addSequential(new DelayedCommand(1,new DriveToDistance(30,true)));
 
+		//turn left
+		addSequential(new PIDTurn(90, true));
+		
+		//drive out of the way of the airship
+		addSequential(new DriveToDistance(36, false));
+		
+		//turn right
+		addSequential(new PIDTurn(90, false));
+		
+		//drive towards gear station
+		addSequential(new DriveToDistance(120, false));
 	}
 }
