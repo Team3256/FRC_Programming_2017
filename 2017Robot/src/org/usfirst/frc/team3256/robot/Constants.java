186f95643634445f95973d9126c277a13336638d
Team 3256 <warriorborgs@gmail.com>
2017-04-07 15:10:08 -0700

    Commit before pull

--- 2017Robot/src/org/usfirst/frc/team3256/robot/Constants.java
+++ 2017Robot/src/org/usfirst/frc/team3256/robot/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 	public static final int HANGER = 7;
 	public static final int INNER_MOTOR_ROLLER = 4;	
 	public static final int OUTER_MOTOR_ROLLER = 3; 
-	public static final int GEAR_ROLLER = 0;
+	public static final int GEAR_ROLLER = 3;
 	
 	// CAN Ports (for CAN Talons)
 	public static final int GEAR_INTAKE_PIVOT = 4; // TODO: set actual CAN port 
